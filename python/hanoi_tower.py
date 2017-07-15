045320267bc5cc226b4bb283d945aad24b4dcef7
12ff806 <12ff806@protonmail.com>
2017-07-15 14:21:40 +0800

    hanoi tower

---
+++ python/hanoi_tower.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def hanoi(n, a, b, c):
+    if n == 1:
+        print("move", a, "--->", c)
+    else:
+        hanoi(n-1, a, c, b)
+        hanoi(1, a, b, c)
+        hanoi(n-1, b, a, c)
+
+
+hanoi(3, "A", "B", "C")
