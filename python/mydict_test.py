73ce290b51e77b0e6bda88c81108cab885201cfc
12ff806 <12ff806@protonmail.com>
2017-07-20 22:59:24 +0800

    unit test

--- python/mydict_test.py
+++ python/mydict_test.py
@@ -6,8 +6,19 @@ import unittest
 from mydict import Dict
 
 
+# 测试类
 class TestDict(unittest.TestCase):
 
+    # 可以在单元测试中编写两个特殊的setUp()和tearDown()方法
+    # 这两个方法会分别在每调用一个测试方法的前后分别被执行
+    def setUp(self):
+        print("setUp...")
+
+    def tearDown(self):
+        print("tearDown...")
+
+    # 以test开头的方法就是测试方法
+    # 不以test开头的方法不被认为是测试方法，测试的时候不会被执行
     def test_init(self):
         d = Dict(a=1, b="test")
         self.assertEqual(d.a, 1)
@@ -35,5 +46,10 @@ class TestDict(unittest.TestCase):
         with self.assertRaises(AttributeError):
             value = d.empty
 
+# 这是最简单的运行单元测试的方式, 这样就可以把mydict_test.py当做正常的python脚本运行
+# 另一种方法是在命令行通过参数-m unittest直接运行单元测试: python3 -m unittest mydict_test
+# 这是推荐的做法，因为这样可以一次批量运行很多单元测试，并且，有很多工具可以自动来运行这些单元测试
 if __name__ == "__main__":
     unittest.main()
+
+
