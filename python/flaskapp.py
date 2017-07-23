a9ee98c523709f407640b7999b82d3632bd3f76e
12ff806 <12ff806@protonmail.com>
2017-07-23 09:53:59 +0800

    flask app

--- python/flaskapp.py
+++ python/flaskapp.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
-from flask import Flask
-from flask import request
+from flask import Flask, request, render_template
 
 
 app = Flask(__name__)
@@ -11,25 +10,20 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    return """<h1>Home</h1>
-              <a href="/signin">Sign In</a>
-              """
-
+    return render_template("flaskapp_home.html")
 
 @app.route("/signin", methods=["GET"])
 def signin_form():
-    return """<form action="/signin" method="post">
-              <p><input name="username"></p>
-              <p><input name="password" type="password"></p>
-              <p><button type="submit">Sign In</button></p>
-              </form>"""
+    return render_template("flaskapp_form.html")
 
 
 @app.route("/signin", methods=["POST"])
 def signin():
-    if request.form["username"] == "admin" and request.form["password"] == "password":
-        return "<h3>Hello, admin!</h3>"
-    return "<h3>Bad username or password</h3>"
+    username = request.form["username"]
+    password = request.form["password"]
+    if username == "admin" and password == "password":
+        return render_template("flaskapp_signin_ok.html", username = username)
+    return render_template("flaskapp_form.html", message = "Bad username or password", username = username)
 
 
 if __name__ == "__main__":
