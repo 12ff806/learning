8282db7fda09ebb59ffbead1f9ab6a12788dab46
12ff806 <12ff806@protonmail.com>
2017-08-18 11:01:57 +0800

    web form

--- flask/hello.py
+++ flask/hello.py
@@ -2,22 +2,30 @@
 # -*- coding: utf-8 -*-
 
 
-from flask import Flask
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask import request
 from flask import make_response
-from flask import redirect
 from flask import abort
-from flask import render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 from datetime import datetime
 
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "hard to guess string"
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+
+class NameForm(FlaskForm):
+    name = StringField("What is your name?", validators=[Required()])
+    submit = SubmitField("Submit")
 
 
 class User(object):
@@ -49,9 +57,21 @@ def page_not_found(e):
     return render_template("500.html"), 500
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html", current_time=datetime.utcnow()) 
+    #name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        #name = form.name.data
+        #form.name.data = ""
+        old_name = session.get("name")
+        if old_name is not None and old_name != form.name.data:
+            flash("Looks like you have changed your name!")
+        session["name"] = form.name.data
+        return redirect(url_for("index"))
+    #return render_template("index.html", form=form, name=name, current_time=datetime.utcnow()) 
+    return render_template("index.html",
+        form=form, name=session.get("name"), current_time=datetime.utcnow()) 
 
     #return redirect("/user/1")
 
