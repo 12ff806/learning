34e68eeb4574525d875f5da879a63e02221dda99
Sin <12ff806@protonmail.com>
2017-08-19 10:13:29 +0800

    email

--- flask/hello.py
+++ flask/hello.py
@@ -3,6 +3,8 @@
 
 
 import os
+from datetime import datetime
+from threading import Thread
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask import request
 from flask import make_response
@@ -14,8 +16,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
 from flask_migrate import Migrate, MigrateCommand
-from datetime import datetime
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -24,17 +26,49 @@ app.config["SECRET_KEY"] = "hard to guess string"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "data.sqlite")
 app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["MAIL_SERVER"] = "smtp.163.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+app.config["FLASKAPP_MAIL_SUBJECT_PREFIX"] = "[FlaskApp]"
+app.config["FLASKAPP_MAIL_SENDER"] = "FlaskApp Admin <Janus_Zhao@163.com>"
+app.config["FLASKAPP_ADMIN"] = os.environ.get("FLASKAPP_ADMIN")
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+mail = Mail(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
 
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command("db", MigrateCommand)
+
+
+def send_async_mail(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+def send_mail(to, subject, template, **kwargs):
+    msg = Message(app.config["FLASKAPP_MAIL_SUBJECT_PREFIX"] + subject,
+                  sender=app.config["FLASKAPP_MAIL_SENDER"], recipients=[to])
+    msg.body = render_template(template + ".txt", **kwargs)
+    msg.html = render_template(template + ".html", **kwargs)
+    thr = Thread(target=send_async_mail, args=[app, msg])
+    thr.start()
+    return thr
+
+
+#def send_mail(to, subject, template, **kwargs):
+#    msg = Message(app.config["FLASKAPP_MAIL_SUBJECT_PREFIX"] + subject,
+#                  sender=app.config["FLASKAPP_MAIL_SENDER"], recipients=[to])
+#    msg.body = render_template(template + ".txt", **kwargs)
+#    msg.html = render_template(template + ".html", **kwargs)
+#    mail.send(msg)
 
 
 class Role(db.Model):
@@ -83,9 +117,12 @@ def index():
             db.session.add(user)
             db.session.commit()
             session["known"] = False
+            if app.config["FLASKAPP_ADMIN"]:
+                send_mail(app.config["FLASKAPP_ADMIN"], "New User", "mail/new_user", user=user)
         else:
             session["known"] = True
         session["name"] = form.name.data
+        form.name.data = ""
         return redirect(url_for("index"))
     return render_template("index.html",
         form=form, name=session.get("name"),
