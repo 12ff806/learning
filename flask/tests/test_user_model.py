e87ea4cb5e93c12001846cc0d0a5d86ea149bb60
Sin <12ff806@protonmail.com>
2017-08-23 13:54:25 +0800

    profile

--- flask/tests/test_user_model.py
+++ flask/tests/test_user_model.py
@@ -3,6 +3,7 @@
 
 import time
 import unittest
+from datetime import datetime
 from app import create_app, db
 from app.models import User, AnonymousUser, Role, Permission
 
@@ -117,5 +118,37 @@ class UserModelTestCase(unittest.TestCase):
     def test_anonymous_user(self):
         u = AnonymousUser()
         self.assertFalse(u.can(Permission.FOLLOW))
+
+    def test_timestamps(self):
+        u = User(password="cat")
+        db.session.add(u)
+        db.session.commit()
+        self.assertTrue((datetime.utcnow() - u.member_since).total_seconds() < 3)
+        self.assertTrue((datetime.utcnow() - u.last_seen).total_seconds() < 3)
+
+    def test_ping(self):
+        u = User(password="cat")
+        db.session.add(u)
+        db.session.commit()
+        old_last_seen = u.last_seen
+        u.ping()
+        self.assertTrue(u.last_seen > old_last_seen)
+
+    def test_gravatar(self):
+        u = User(email="test@test.com", password="cat")
+        with self.app.test_request_context("/"):
+            gravatar = u.gravatar()
+            gravatar_256 = u.gravatar(size=256)
+            gravatar_pg = u.gravatar(rating="pg")
+            gravatar_retro = u.gravatar(default="retro")
+        with self.app.test_request_context("/", base_url="https://example.com"):
+            gravatar_ssl = u.gravatar()
+        self.assertTrue("http://www.gravatar.com/avatar/" + 
+                        "b642b4217b34b1e8d3bd915fc65c4452" in gravatar)
+        self.assertTrue("s=256" in gravatar_256)
+        self.assertTrue("r=pg" in gravatar_pg)
+        self.assertTrue("d=retro" in gravatar_retro)
+        self.assertTrue("https://secure.gravatar.com/avatar/" + 
+                        "b642b4217b34b1e8d3bd915fc65c4452" in gravatar_ssl)
 
 
