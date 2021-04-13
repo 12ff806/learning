bbd6480ce3325fbb6804681e276e976da845d87b
12ff806 <12ff806@protonmail.com>
2021-04-13 22:14:53 +0800

    update

---
+++ data_structures_and_algorithm_analysis_in_c/chapter_03/list.c
@@ -0,0 +1,66 @@
+/*
+ * 12ff806
+ * 2021 04 13
+ */
+
+#include "list.h"
+
+
+/* node implementation */
+struct node
+{
+    element_type element;
+    position     next;
+}
+
+
+/* Return true if l is empty */
+int is_empty(list l)
+{
+    return l->next == NULL;
+}
+
+
+/* Return true if p is the last position in list l */
+/* parameter l is unused in this implementation */
+int is_last(position p, list l) {
+    return p->next == NULL;
+}
+
+
+/* Return position of x in l, NULL if not found */
+position find(element_type x, list l) {
+    position p;
+    
+    p = l->next;
+    while(p != NULL && p->element != x) {
+        p = p->next;
+    }
+    return p;
+}
+
+
+/* Delete first occurrence of x from a list */
+/* Assume use of a header node */
+void delete(element_type x, list l) {
+    position p, tmp_cell;
+
+    p = find_previous(x, l);
+    /* Assumption of header use, x is found, delete it */
+    if(!is_last(p, l)) {
+        tmp_cell = p->next;
+        p->next = tmp_cell->next;
+        free(tmp_cell);
+    }
+}
+
+
+/* If x is not found, then next field of returned */
+/* position is NULL */
+/* Assumes a header */
+position find_previous(element_type x, list l) {
+    position p;
+    
+    p = l;
+    while(p->next != )
+}
